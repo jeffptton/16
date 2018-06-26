63c57291bbc10b3e9b65b3736fed6c0caaa08cd5
jeffptton <39930443+jeffptton@users.noreply.github.com>
2018-06-26 18:53:18 -0500

    Add files via upload

---
+++ SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStory
+{
+    internal class SuperHero
+    {
+        internal int _specialty;
+        internal object _strength;
+
+        public SuperHero(string hERO, int v1, int v2, string v3)
+        {
+        }
+    }
+}
